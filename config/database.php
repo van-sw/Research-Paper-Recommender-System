6ef08bf72f4f58b0f12aef85017dcba9e216078d
Evan <evanwidjaja5@gmail.com>
2026-04-20 22:14:26 +0700

    Update

--- config/database.php
+++ config/database.php
@@ -87,7 +87,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_SOCKET') ? "/cloudsql/" . env('DB_SOCKET') : env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
