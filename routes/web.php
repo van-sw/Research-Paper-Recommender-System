db54898773ad265e5b4b3b7d10503a452a2438f2
Evan <evanwidjaja5@gmail.com>
2026-04-20 22:25:16 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,3 @@ use App\Http\Controllers\PaperController;
 
 Route::get('/', [PaperController::class, 'index'])->name('papers.index');
 Route::get('/papers/{id}', [PaperController::class, 'show'])->name('papers.show');
-
-Route::get('/deploy-migrate', function () {
-    try {
-        Artisan::call('migrate', ['--force' => true]);
-        return "Migration successful: " . Artisan::output();
-    } catch (\Exception $e) {
-        return "Error: " . $e->getMessage();
-    }
-});
